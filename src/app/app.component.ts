4c830b76b8b4679b75c7ce665ed1a994815e2c83
Sylwia <sylwiaszymanska2306@gmail.com>
2025-02-12 15:11:45 +0100

    app.component.ts modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { CommonFunctionsComponent } from './common-functions/common-functions.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonFunctionsComponent],
+  imports: [CommonFunctionsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

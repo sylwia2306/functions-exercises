4ff12f7ea63e8e33288c125f71b29c602f9d846f
Sylwia <sylwiaszymanska2306@gmail.com>
2025-02-12 15:11:08 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonFunctionsComponent } from './common-functions/common-functions.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonFunctionsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/common-functions/common-functions.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+
+@Component({
+    standalone: true,
+    selector: 'app-common-functions',
+    imports: [CommonModule],
+    templateUrl: './common-functions.component.html',
+    styleUrl: './common-functions.component.scss'
+})
+
+export class CommonFunctionsComponent {
+
+}
